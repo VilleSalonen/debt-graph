b04e7e4740c3f39daa755df556fc632efb6d8697
Ville Salonen <ville.salonen@iki.fi>
2014-09-09 19:41:23 +0300

    Improved registration and logging in
    - Added better styling and moved logging in and out to the top of the page.
    - Added Gravatar support.

--- client/client.js
+++ client/client.js
@@ -2,31 +2,26 @@ Template.users.users = function () {
   return Meteor.users.find();
 };
 
+Template.registration.isLoggedIn = function () {
+  return Meteor.userId();
+};
+
 Template.login.isLoggedIn = function () {
   return Meteor.userId();
+};
+
+Template.login.gravatar = function () {
+  var user = Meteor.user();
+  if (!user) { return ""; }
+
+  return Meteor.user().profile.gravatar;
 };
 
 Template.login.accounts = function () {
   return Meteor.user().profile.accounts;
 };
 
-Template.login.events({
-  'submit #login-form': function(e, t) {
-    e.preventDefault();
-    
-    // retrieve the input field values
-    var email = t.find('#login-email').value
-    , password = t.find('#login-password').value;
-
-    Meteor.loginWithPassword(email, password, function(err) {
-      if (err)
-        alert(err);
-    });
-    
-    // Prevent form submission
-    return false; 
-  },
-  
+Template.registration.events({
   'submit #registration-form': function(e, t) {
     e.preventDefault();
     
@@ -37,7 +32,7 @@ Template.login.events({
 
     var gravatar = CryptoJS.MD5(email).toString();
 
-    Accounts.createUser({ username: username, email: email, password: password, profile: { accounts: [], gravatar: "gravatar" } }, function(err) {
+    Accounts.createUser({ username: username, email: email, password: password, profile: { accounts: [], gravatar: gravatar } }, function(err) {
       if (err)
         alert(err);
     });
@@ -45,7 +40,9 @@ Template.login.events({
     // Prevent form submission
     return false; 
   },
-  
+});
+
+Template.accounts.events({
   'submit #account-form': function(e, t) {
     e.preventDefault();
     
@@ -66,10 +63,6 @@ Template.login.events({
     return false; 
   },
   
-  'click .logout': function (e, t) {
-    Meteor.logout();
-  },
-  
   'click .remove-account': function (e, t) {
     var unwantedAccount = this;
     var accounts = Meteor.user().profile.accounts;
@@ -80,4 +73,26 @@ Template.login.events({
     
     Meteor.users.update({ _id: Meteor.userId() }, { $set: { profile: { accounts: accountsLeft } } });
   }
+});
+
+Template.login.events({
+  'submit #login-form': function(e, t) {
+    e.preventDefault();
+    
+    // retrieve the input field values
+    var email = t.find('#login-email').value
+    , password = t.find('#login-password').value;
+
+    Meteor.loginWithPassword(email, password, function(err) {
+      if (err)
+        alert(err);
+    });
+    
+    // Prevent form submission
+    return false; 
+  },
+  
+  'click .logout': function (e, t) {
+    Meteor.logout();
+  },
 });
